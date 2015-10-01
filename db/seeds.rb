8ac4e09370816f9fb295b550444dad135d2f1bf0
NTKIEN92 <ntkien92@gmail.com>
2015-10-01 15:31:51 +0700

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.create!(name: "Example User",
 							password_confirmation: "foobar",
 							admin: true,
 							activated: true,
-             activated_at: Time.zone.now)
+              activated_at: Time.zone.now)
 99.times do |n|
 	name = Faker::Name.name
 	email = "example-#{n+1}@railstutorial.org"
@@ -28,3 +28,10 @@ users = User.order(:created_at).take(6)
 	content = Faker::Lorem.sentence(5)
 	users.each { |user| user.microposts.create!(content: content)}
 end
+# Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,7 +15,7 @@ class UsersControllerTest < ActionController::TestCase
 		assert_redirected_to login_url
 	end
 	test "should redirect update when not logged in" do
-		
+
 		patch :update, id: @user, user: {name: @user.name, email: @user.email}
 		assert_not flash.empty?
 		assert_redirected_to login_url
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionController::TestCase
     assert flash.empty?
     assert_redirected_to root_url
   end
-  
+
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
@@ -53,5 +53,13 @@ class UsersControllerTest < ActionController::TestCase
       delete :destroy, id: @user
     end
     assert_redirected_to root_url
-  end  	
+  end
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
 end
